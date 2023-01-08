44487bd377412bbb653926d63e0f6ead9014ef19
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2023-01-08 23:03:50 +0200

    Add LIBCLANG_PATH environment variable usage for loading, add deno.json and deno.lock

--- README.md
+++ README.md
@@ -13,6 +13,27 @@ complete with comment JSDoc'ifying. Check out
 [https://github.com/aapoalas/libclang-deno/blob/main/build/build.ts](build.ts)
 for the ugly details.
 
+## Starting up
+
+To find `libclang`, the library uses the `LIBCLANG_PATH` environment variable.
+This variable can either contain the direct path to a libclang shared library or
+a path to the directory containing the shared library. To read the environment
+variable the library naturally needs the `--allow-env=LIBCLANG_PATH` Deno
+permissions flag.
+
+Additionally, as with all FFI libraries at this moment, the library of course
+needs the `--unstable` and `--allow-ffi` flags. Unfortunately, at present it is
+not enough to give a limited FFI permission flag to just the libclang shared
+library, eg. `--allow-ffi=/lib64/libclang.so.14.0.6` as the library needs to use
+some Deno FFI APIs beyond `dlopen`, and those currently require unrestricted FFI
+permissions.
+
+An example startup:
+
+```sh
+LIBCLANG_PATH=/lib64 deno run --unstable --allow-env=LIBCLANG_PATH --allow-ffi lib/mod.ts
+```
+
 ### Entry points and memory management
 
 The main entry point into the libclang API is the `CXIndex` and its
--- lib/ffi.ts
+++ lib/ffi.ts
@@ -1,3 +1,4 @@
+import { join } from "https://deno.land/std@0.170.0/path/mod.ts";
 import * as BuildSystem from "./include/BuildSystem.h.ts";
 import * as CXCompilationDatabase from "./include/CXCompilationDatabase.h.ts";
 import * as CXDiagnostic from "./include/CXDiagnostic.h.ts";
@@ -9,18 +10,51 @@ import * as FatalErrorHandler from "./include/FatalErrorHandler.h.ts";
 import * as Index from "./include/Index.h.ts";
 import * as Rewrite from "./include/Rewrite.h.ts";
 
-export const libclang = Deno.dlopen(
-  "/lib64/libclang.so.14.0.6",
-  {
-    ...BuildSystem,
-    ...CXCompilationDatabase,
-    ...CXDiagnostic,
-    ...CXFile,
-    ...CXSourceLocation,
-    ...CXString,
-    ...Documentation,
-    ...FatalErrorHandler,
-    ...Index,
-    ...Rewrite,
-  } as const,
-);
+const IMPORTS = {
+  ...BuildSystem,
+  ...CXCompilationDatabase,
+  ...CXDiagnostic,
+  ...CXFile,
+  ...CXSourceLocation,
+  ...CXString,
+  ...Documentation,
+  ...FatalErrorHandler,
+  ...Index,
+  ...Rewrite,
+} as const;
+
+const libclangPath = Deno.env.get("LIBCLANG_PATH");
+
+if (!libclangPath) {
+  throw new Error(
+    "Cannot load libclang without LIBCLANG_PATH environment variable",
+  );
+}
+
+let libclang: ReturnType<typeof Deno.dlopen<typeof IMPORTS>>;
+
+if (libclangPath.includes(".so") || libclangPath.endsWith(".dll")) {
+  // Explicit path, try without catching
+  libclang = Deno.dlopen(libclangPath, IMPORTS);
+} else if (Deno.build.os === "windows") {
+  libclang = Deno.dlopen(
+    join(libclangPath, "libclang.dll"),
+    IMPORTS,
+  );
+} else {
+  // Try 14.0.6 first, then 14, then 13
+  try {
+    libclang = Deno.dlopen(
+      join(libclangPath, "libclang.so.14.0.6"),
+      IMPORTS,
+    );
+  } catch {
+    try {
+      libclang = Deno.dlopen(join(libclangPath, "libclang.so.14"), IMPORTS);
+    } catch {
+      libclang = Deno.dlopen(join(libclangPath, "libclang.so.13"), IMPORTS);
+    }
+  }
+}
+
+export { libclang };
